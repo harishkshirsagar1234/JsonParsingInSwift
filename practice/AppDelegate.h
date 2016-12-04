0645a44dd1470935b483f2f194b68d216fbde09c
Harish <harish@harishs-mac-pro.local>
2016-12-04 18:20:30 +0530

    Initial Commit

---
+++ practice/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  practice
+//
+//  Created by Harish on 04/12/16.
+//  Copyright © 2016 Harish. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ practice/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  practice
+//
+//  Created by Harish on 04/12/16.
+//  Copyright © 2016 Harish. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
